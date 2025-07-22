8d2393ccf30040446f10e6c1e0f7e6d4d918f0bb
vanya642004 <139208978+vanya642004@users.noreply.github.com>
2025-07-22 17:15:48 +0530

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,14 +1,21 @@
 import plotly.express as px
 
 def generate_chart(question, df):
+    cols = df.columns.str.lower().tolist()
     question = question.lower()
-    if "sales" in question and "product" in question and "total" in question:
-        return px.bar(df, x="item_id", y="total_sales", title="Total Sales per Product")
-    if "clicks" in question:
-        return px.line(df, x="date", y="clicks", title="Clicks Over Time")
-    if "impressions" in question:
-        return px.area(df, x="date", y="impressions", title="Ad Impressions Over Time")
-    if "roas" in question:
-        if "item_id" in df.columns:
+
+    try:
+        if "sales" in question and "item" in question and "total" in question:
+            if "item_id" in cols and "total_sales" in cols:
+                return px.bar(df, x="item_id", y="total_sales", title="Total Sales per Product")
+        elif "clicks" in question and "date" in cols:
+            return px.line(df, x="date", y="clicks", title="Clicks Over Time")
+        elif "impressions" in question and "date" in cols:
+            return px.area(df, x="date", y="impressions", title="Impressions Over Time")
+        elif "roas" in question and "ad_sales" in cols and "ad_spend" in cols:
             return px.scatter(df, x="item_id", y="ad_sales", size="ad_spend", title="RoAS Bubble Chart")
-    return None
+        else:
+            return None
+    except Exception as e:
+        print("Chart generation error:", e)
+        return None
